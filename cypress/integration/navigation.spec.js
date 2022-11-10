5f1ec51454b72cd9785d0e0f84de53686365d984
Luke Li <lukeli@Lukes-MacBook-Pro.local>
2022-11-10 00:17:03 -0800

    complete test on Navigating Days using cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,7 +5,13 @@ describe("Navigation", () => {
 
 
   it("should navigate to Tuesday", () => {
-    cy.get("li").contains("Tuesday").click();
+    cy.visit("/");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
   });
+
+  
 
 });
