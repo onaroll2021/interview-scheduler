c003663c4b38930dc5eaa02b931d5c9ebb8257c1
Luke Li <lukeli@Lukes-MacBook-Pro.local>
2022-11-10 00:08:11 -0800

    implement cypress test navigate to Tuesday

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,10 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+
+  it("should navigate to Tuesday", () => {
+    cy.get("li").contains("Tuesday").click();
+  });
+
 });
